4aa02db9dc7e0491898ee461be9bbf4e049be862
Shawn Taylor <shawn@mapsandapps.net>
2019-08-25 08:03:54 -0400

    Build out Box Pusher from https://www.youtube.com/watch?v=ESh8phnmiXg tutorial

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public bool m_OnTarget;
+
+    public bool Move(Vector2 direction) // avoid ability to move diagonally
+    {
+        if (BoxBlocked(transform.position, direction))
+        {
+            return false;
+        }
+        else
+        {
+            transform.Translate(direction);
+            TestForOnTarget();
+            return true;
+        }
+    }
+
+    bool BoxBlocked(Vector3 position, Vector2 direction)
+    {
+        Vector2 newPos = new Vector2(position.x, position.y) + direction;
+        GameObject[] walls = GameObject.FindGameObjectsWithTag("Wall");
+
+        foreach (var wall in walls)
+        {
+            if (wall.transform.position.x == newPos.x && wall.transform.position.y == newPos.y)
+            {
+                return true;
+            }
+        }
+
+        GameObject[] boxes = GameObject.FindGameObjectsWithTag("Box");
+
+        foreach (var box in boxes)
+        {
+            if (box.transform.position.x == newPos.x && box.transform.position.y == newPos.y)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void TestForOnTarget()
+    {
+        GameObject[] crosses = GameObject.FindGameObjectsWithTag("Target");
+        foreach (var cross in crosses)
+        {
+            if (transform.position.x == cross.transform.position.x && transform.position.y == cross.transform.position.y)
+            {
+                GetComponent<SpriteRenderer>().color = Color.red;
+                m_OnTarget = true;
+                return;
+            }
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+        m_OnTarget = false;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public LevelBuilder m_LevelBuilder;
+    public GameObject m_NextButton; 
+    private bool m_ReadyForInput;
+    private Player m_Player;
+
+    void Start()
+    {
+        m_NextButton.SetActive(false);
+        ResetScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveInput.Normalize();
+
+        if (moveInput.sqrMagnitude > 0.5) // button pressed or held
+        {
+            if (m_ReadyForInput)
+            {
+                m_ReadyForInput = false;
+                m_Player?.Move(moveInput);
+                m_NextButton.SetActive(IsLevelComplete());
+            }
+        }
+        else
+        {
+            m_ReadyForInput = true;
+        }
+    }
+
+    public void NextLevel()
+    {
+        m_NextButton.SetActive(false);
+        m_LevelBuilder.NextLevel();
+        StartCoroutine(ResetSceneAsync());
+    }
+
+    public void ResetScene()
+    {
+        StartCoroutine(ResetSceneAsync());
+    }
+
+    bool IsLevelComplete()
+    {
+        Box[] boxes = FindObjectsOfType<Box>();
+        foreach (var box in boxes)
+        {
+            if (!box.m_OnTarget) return false;
+        }
+        return true;
+    }
+
+    IEnumerator ResetSceneAsync()
+    {
+        if (SceneManager.sceneCount > 1)
+        {
+            AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync("LevelScene");
+            while(!asyncUnload.isDone)
+            {
+                yield return null;
+                Debug.Log("Unloading...");
+            }
+            Debug.Log("Unload done");
+            Resources.UnloadUnusedAssets();
+        }
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("LevelScene", LoadSceneMode.Additive);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+            Debug.Log("Loading...");
+        }
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("LevelScene"));
+        m_LevelBuilder.Build();
+        m_Player = FindObjectOfType<Player>();
+        Debug.Log("Level loaded");
+    }
+}
